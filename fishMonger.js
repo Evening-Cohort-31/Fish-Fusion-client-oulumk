49eb75098e5f50a3c2d21b80a37df48c19062fda
Caleb Pittman <ambassadoor.dev@gmail.com>
2025-07-17 19:19:08 -0500

    Added auto purchase rare fish functionality

--- fishMonger.js
+++ fishMonger.js
@@ -1,8 +1,8 @@
-const {boatInventory} = require('./fishingBoat.js');
+const {generateInventory} = require('./fishingBoat.js');
 
 // Filters fish inventory by monger's and chef's requirements and returns results
 function mongerInventory(maxPrice) {
-    let inventory = boatInventory();
+    let inventory = generateInventory();
 
     let boatMessage = "The fishing boat has caught:\n"
     for (fish of inventory) {
@@ -14,9 +14,22 @@ function mongerInventory(maxPrice) {
     let mongerMessage = "The Fishmonger has purchased:\n"
     let purchasedFish = [];
 
-    purchasedFish = inventory.flatMap(fish => 
-        fish.amount >= 10 && fish.price < 7.50 ? (fish.amount -= 10, mongerMessage += `10 ${fish.species}\n`,{...fish, amount: 10}) : []
-    )
+    for (let x = 0; x < inventory.length; x ++) {
+        let fish = inventory[x]
+        if (fish.rare) {
+            purchasedFish.push({...fish})
+            mongerMessage += `${fish.amount} ${fish.species}\n`
+            inventory.splice(x,1)
+        }
+        else if (fish.amount >= 10 && fish.price < 7.50) {
+            purchasedFish.push({...fish, amount: 10})
+            mongerMessage += `10 ${fish.species}\n`
+            fish.amount -= 10
+        }
+    }
+    // purchasedFish = inventory.flatMap(fish => 
+    //     fish.rare || (fish.amount >= 10 && fish.price) < 7.50 ? (fish.amount -= 10, mongerMessage += `10 ${fish.species}\n`,{...fish, amount: 10}) : []
+    // )
 
     console.log(mongerMessage)
     let updatedBoatMessage = "The fishing boat now has:\n"
@@ -47,9 +60,24 @@ function mongerInventory(maxPrice) {
     //     }
     // }
 
-    restaurantInventory = purchasedFish.flatMap(fish =>
-        fish.price <= maxPrice ? (fish.amount -= Math.ceil(fish.amount/2), restaurantMessage += `${fish.amount} ${fish.species}\n`, {...fish, amount: Math.ceil(fish.amount/2)}) : []
-    )
+
+    for (let x = 0; x < purchasedFish.length; x ++) {
+        let fish = purchasedFish[x];
+        if (fish.rare) {
+            restaurantInventory.push(fish);
+            restaurantMessage += `${fish.amount} ${fish.species}\n`
+            purchasedFish.splice(x,1);
+        } else if (fish.price <= maxPrice) {
+            let purchasedAmount = Math.ceil(fish.amount/2);
+            restaurantInventory.push({...fish, amount: purchasedAmount});
+            restaurantMessage += `${purchasedAmount} ${fish.species}\n`;
+            fish.amount -= purchasedAmount;
+        }
+    }
+
+    // restaurantInventory = purchasedFish.flatMap(fish =>
+    //     fish.price <= maxPrice ? (fish.amount -= Math.ceil(fish.amount/2), restaurantMessage += `${fish.amount} ${fish.species}\n`, {...fish, amount: Math.ceil(fish.amount/2)}) : []
+    // )
 
     console.log(restaurantMessage)
 
